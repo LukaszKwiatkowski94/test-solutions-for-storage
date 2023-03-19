ff16e79193cc79ca0a7c9c97592d527dd62521e9
BuildTools <unconfigured@null.spigotmc.org>
2023-03-19 13:28:34 +0100

    Add vue json

---
+++ VUE/JSON in VUE/component.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <ul>
+      <li v-for="item in items" :key="item.id">{{ item.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import data from './data.json'
+
+export default {
+  name: 'MyComponent',
+  props: {
+    title: String,
+    items: Array
+  },
+  mounted () {
+    // przypisanie danych z pliku JSON do właściwości komponentu
+    this.title = data.title
+    this.items = data.items
+  }
+}
+</script>
