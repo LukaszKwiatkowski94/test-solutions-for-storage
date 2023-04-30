5d807188b907ec12255b0985eade8632773423c6
BuildTools <unconfigured@null.spigotmc.org>
2023-04-30 23:22:33 +0200

    Added html from string to view

---
+++ VUE/HTML from String/component.vue
@@ -0,0 +1,13 @@
+<template>
+  <div v-html="htmlString"></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      htmlString: '<h1>Witaj świecie!</h1><p>To jest tekst z <strong>tagami HTML</strong></p>'
+    }
+  }
+}
+</script>
